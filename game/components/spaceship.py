8f5f34676f4d0bfffc48544fa73baf64b113899d
Michaelec19 <Michael.Espinal@fundacion-jala.org>
2023-06-13 01:29:48 -0500

    Activity 1: Left and Right Limit
    Se quitaron las barreras horizontales y se hizo que cuando se atravesara cierto punto iniciara en el espacio complemente opuesto. SIN SALTOS.

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -15,19 +15,27 @@ class Spaceship(Sprite):
         if user_input[pygame.K_LEFT]:
             self.move_left()
         elif user_input[pygame.K_RIGHT]:
-            self.move_rigth()
+            self.move_right()
         elif user_input[pygame.K_UP]:
             self.move_up()
         elif user_input[pygame.K_DOWN]:
             self.move_down()
-
+    
     def move_left(self):
         if self.rect.left > 0:
             self.rect.x -= 10
-            
-    def move_rigth(self):
+        elif self.rect.left <= 0 and abs(self.rect.left >= -50 ) :
+            self.rect.x -= 10
+        else:
+            self.rect.x = SCREEN_WIDTH
+        
+    def move_right(self):
         if self.rect.right < SCREEN_WIDTH:
             self.rect.x += 10
+        elif self.rect.left <= SCREEN_WIDTH and abs(self.rect.left - SCREEN_WIDTH) >= 1:
+            self.rect.x += 10
+        else: 
+            self.rect.x = (SCREEN_WIDTH) - (SCREEN_WIDTH - -50)
 
     def move_up(self):
         if self.rect.y > SCREEN_HEIGHT // 2:
