281ecc248a35f9ea76686f3bca2f0c763fa2c335
Michaelec19 <Michael.Espinal@fundacion-jala.org>
2023-06-14 18:14:29 -0500

    Activity 2: implementacion Timer

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,20 +1,37 @@
+import time
 from game.components.enemies.enemy import Enemy
 from game.components.enemies.enemy_2 import Enemy_2
 from game.components.enemies.meteor import Meteor
+
 
 class EnemyManager:
     def __init__(self):
         self.enemies: list[Enemy] = []
         self.enemies_2: list[Enemy_2] = []
         self.meteors: list[Meteor] = []
+        
+        self.enemy_timer = 0  # Contador de tiempo para el enemigo
+        self.enemy_2_timer = 0 
+        self.meteor_timer = 0  
+
+        self.enemy_delay = 5  # Tiempo de espera en segundos para el enemigo
+        self.enemy_2_delay = 10 
+        self.meteor_delay = 20  
 
     def update(self):
-        if not self.enemies:
+        current_time = time.time() 
+
+        if current_time - self.enemy_timer >= self.enemy_delay:
             self.enemies.append(Enemy())
-        if not self.enemies_2:
+            self.enemy_timer = current_time  # Reiniciar el contador de tiempo
+
+        if not self.enemies_2 and current_time - self.enemy_2_timer >= self.enemy_2_delay:
             self.enemies_2.append(Enemy_2())
-        if not self.meteors:
+            self.enemy_2_timer = current_time 
+
+        if not self.meteors and current_time - self.meteor_timer >= self.meteor_delay:
             self.meteors.append(Meteor())
+            self.meteor_timer = current_time  
 
         for enemy in self.enemies:
             enemy.update(self.enemies)
@@ -34,3 +51,4 @@ class EnemyManager:
 
         for meteor in self.meteors:
             meteor.draw(screen)
+
