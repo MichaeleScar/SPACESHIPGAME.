c94118536533001484fcd118f885e40f2f8eafb5
Michaelec19 <Michael.Espinal@fundacion-jala.org>
2023-06-15 14:54:35 -0500

    Activity 3: Remove Enemies
    Se agrega el remove a enemies(Enemy, Enemy_2, Meteor) cuando lo toca el disparo del jugador.

--- game/components/bullets/bullet_manager.py
+++ game/components/bullets/bullet_manager.py
@@ -1,4 +1,7 @@
 import pygame
+from game.components.enemies.enemy import Enemy
+from game.components.enemies.enemy_2 import Enemy_2
+from game.components.enemies.meteor import Meteor
 from game.components.bullets.bullet import Bullet
 from game.utils.constants import ENEMY_TYPE, PLAYER_TYPE
 
@@ -9,6 +12,7 @@ class BulletManager:
         self.player_bullets: list[Bullet] = []
         self.enemy_bullets: list[Bullet] =[]
 
+
     def update(self, game):
         for bullet in self.enemy_bullets:
             bullet.update(self.enemy_bullets, game.player)
@@ -17,10 +21,32 @@ class BulletManager:
                 game.playing = False
                 pygame.time.delay(1000)
                 break
+
         for bullet in self.player_bullets:
-            bullet.update(self.player_bullets, game.enemy_manager.enemies + game.enemy_manager.enemies_2)
-            if pygame.sprite.collide_rect(bullet, game.enemy):
-                self.player_bullets.remove(bullet)
+            bullet.update(self.player_bullets, game.enemy_manager.enemies + game.enemy_manager.enemies_2 + game.enemy_manager.meteors)
+            for enemy in game.enemy_manager.enemies:
+                if pygame.sprite.collide_rect(bullet, enemy):
+                    if bullet in self.player_bullets:
+                        self.player_bullets.remove(bullet)
+                    if enemy in game.enemy_manager.enemies:
+                        game.enemy_manager.enemies.remove(enemy)
+                    break
+            for enemy_2 in game.enemy_manager.enemies_2:
+                if pygame.sprite.collide_rect(bullet, enemy_2):
+                    if bullet in self.player_bullets:
+                        self.player_bullets.remove(bullet)
+                    if enemy_2 in game.enemy_manager.enemies_2:
+                        game.enemy_manager.enemies_2.remove(enemy_2)
+                    break
+            for meteor in game.enemy_manager.meteors:
+                if pygame.sprite.collide_rect(bullet, meteor):
+                    if bullet in self.player_bullets:
+                        self.player_bullets.remove(bullet)
+                    if meteor in game.enemy_manager.meteors:
+                        game.enemy_manager.meteors.remove(meteor)
+                    break
+
+
 
             #verificar si hemos chocado al jugador
         
